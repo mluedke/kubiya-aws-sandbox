e8b48585cc3b01d0003831211e689c83606e9880
Mike Luedke <mike.luedke@sysdig.com>
2025-02-23 11:47:08 -0800

    test

--- tools/account_tool/account.py
+++ tools/account_tool/account.py
@@ -5,8 +5,10 @@ from kubiya_sdk.tools.registry import tool_registry
 list_accounts = AWSCliTool(
     name="list_accounts",
     description="List accounts",
-    content="aws organizations list-accounts",
-    args=[],
+    content="aws organizations list-accounts --child-type ORGANIZATIONAL-UNIT --parent-id $sandbox-ou",
+    args=[
+        Arg(name="sandbox-ou", type="str", description="Id of OU containing sandbox accounts", reqquired=True),
+    ],
     mermaid_diagram=""
 )
 
